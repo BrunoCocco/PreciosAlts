e3d0e4ef18961ddebfacb25374c78abdb8848a58
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-18 14:28:13 +0100

    agregando monedas al front

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+import viteLogo from "/vite.svg";
+import usePrice from "./hooks/CryptoPrice";
 
-import viteLogo from '/vite.svg'
-import usePrice from "./hooks/CryptoPrice"
-
-import './App.css'
+import "./App.css";
 
 function App() {
   const btc = usePrice("BTC");
-   const xrp = usePrice("XRP");
-   const hbar = usePrice("HBAR");
-
+  const eth = usePrice("ETH");
+  const xrp = usePrice("XRP");
+  const hbar = usePrice("HBAR");
+  const xlm = usePrice("XLM");
 
   return (
     <>
@@ -17,20 +17,17 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-        <p>
-          🟡 Bitcoin: {xrp ? `$${xrp}` : "Cargando..."}
-        </p>
-        <p>
-           Ripple: {btc ? `$${btc}` : "Cargando..."}
-        </p>
-           <p>
-           Hedera: {hbar ? `$${hbar}` : "Cargando..."}
-        </p>
+      <p>🟡 Bitcoin: {btc ? `$${btc}` : "Cargando..."}</p>
+      <p>Ethereum: {eth ? `$${eth}` : "Cargando..."}</p>
+      <p>Ripple: {xrp ? `$${xrp}` : "Cargando..."}</p>
+      <p>Hedera: {hbar ? `$${hbar}` : "Cargando..."}</p>
+      <p>Stellar lummens: {xlm ? `$${xlm}` : "Cargando..."}</p>
       <p className="read-the-docs">
-        Solo a titulo informativo, contiene un delay de 20" no es aconsejable operar siguendo estos indices de precios.
+        Solo a titulo informativo, contiene un delay de 20" no es aconsejable
+        operar siguendo estos indices de precios.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
